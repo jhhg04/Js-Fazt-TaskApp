15af320844eea6aeff77f5fbdf923264c7c18622
jhhg04 <jhhg04@gmail.com>
2021-09-18 09:25:13 -0500

    Finish

--- js/app.js
+++ js/app.js
@@ -20,6 +20,38 @@ function saveTask(e) {
     tasks.push(task);
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }
+
+  getTask();
+
+  document.getElementById("formTask").reset();
 }
 
-function getTask(params) {}
+function getTask() {
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  let tasksView = document.getElementById("tasks");
+  tasksView.innerHTML = "";
+  for (let i = 0; i < tasks.length; i++) {
+    let title = tasks[i].title;
+    let description = tasks[i].description;
+
+    tasksView.innerHTML += `<div class="card mb-3">
+    <div class="card-body">
+      <p>${title} - ${description}</p>
+      <a class="btn btn-danger" onclick="deleteTask('${title}')">Delete</a>
+    </div>
+    </div>`;
+  }
+}
+
+function deleteTask(title) {
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].title == title) {
+      tasks.splice(i, 1);
+    }
+  }
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  getTask();
+}
+
+getTask();
